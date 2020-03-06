029c3b0bc99dad786dbe432ccec38529276ac8bf
guaguaerhao <2809731342@qq.com>
2020-03-06 15:58:47 +0800

    update 添加基于SpringBoot2.0.*的上传文件案例

---
+++ Java/Upload/UploadController.java
@@ -0,0 +1,29 @@
+@Controller
+public class UploadController {
+    // 文件保存路径为 /home/project/upload/ 即当前 project 目录下的 upload 文件夹
+    private final static String FILE_UPLOAD_PATH = "/home/project/upload/";
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败";
+        }
+        String fileName = file.getOriginalFilename();
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        //生成文件名称通用方法
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Random r = new Random();
+        StringBuilder tempName = new StringBuilder();
+        tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
+        String newFileName = tempName.toString();
+        try {
+            // 保存文件
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(FILE_UPLOAD_PATH + newFileName);
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传成功";
+    }
+}
